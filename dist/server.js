47e2142d7d116296ee4abf0cff43b108d8d0673a
Elmer Balbin <contact@elmer.dev>
2025-12-12 11:32:04 -0800

    update server.js and package.json

--- dist/server.js
+++ dist/server.js
@@ -3,16 +3,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const page_routes_1 = __importDefault(require("./routes/page.routes"));
-const express_1 = __importDefault(require("express"));
-const helmet_1 = __importDefault(require("helmet"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const express_1 = __importDefault(require("express"));
+const helmet_1 = __importDefault(require("helmet"));
+const page_routes_1 = __importDefault(require("./routes/page.routes"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use((0, helmet_1.default)());
 app.use((0, cors_1.default)());
-app.use("/contact", page_routes_1.default);
+app.use('/contact-us', page_routes_1.default);
 app.get('/', (_req, res) => {
     res.status(200).send('Welcome to my server!');
 });
